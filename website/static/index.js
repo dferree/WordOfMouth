368b9e44514502334774be90b0e99d9439ec9ab8
dferree <dan.ferree94@gmail.com>
2024-01-31 18:49:46 +0000

    sign in and register page styling

--- website/static/index.js
+++ website/static/index.js
@@ -7,6 +7,20 @@ function deleteActivity(activityId) {
     });
 }
 
-function menuChange(x) {
-    x.classList.toggle("change");
-  }
+function toggleDropdown(category) {
+    var dropdown = document.getElementById(category + "-dropdown");
+    dropdown.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.drop-arrow')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
